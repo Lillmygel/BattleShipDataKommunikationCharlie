c4a0b66a5e0ccdc29ee2af5bb342ed86b9e229aa
Charlie Rydholm <31844668+Lillmygel@users.noreply.github.com>
2018-12-08 19:15:56 +0100

    Made some of the design

---
+++ BattleShipDataKommunikationCharlie/Models/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipDataKommunikationCharlie.Models
+{
+    public class Player : INotifyPropertyChanged
+    {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+                    }
+                }
+            }
+        }
+
+        private string _HostAddress;
+        public string HostAddress
+        {
+            get { return _HostAddress; }
+            set
+            {
+                if (_HostAddress != value)
+                {
+                    _HostAddress = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("HostAddress"));
+                    }
+                }
+            }
+        }
+
+        private string _Port;
+        public string Port
+        {
+            get { return _Port; }
+            set
+            {
+                if (_Port != value)
+                {
+                    _Port = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Port"));
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BattleShipDataKommunikationCharlie/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BattleShipDataKommunikationCharlie.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private SolidColorBrush[,] _MyPlayField;
+        public SolidColorBrush[,] MyPlayField
+        {
+            get { return _MyPlayField; }
+            set
+            {
+                if (_MyPlayField != value)
+                {
+                    _MyPlayField = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("MyPlayField"));
+                    }
+                }
+            }
+        }
+
+        private SolidColorBrush[,] _OpponentsPlayField;
+        public SolidColorBrush[,] OpponentsPlayField
+        {
+            get { return _OpponentsPlayField; }
+            set
+            {
+                if (_OpponentsPlayField != value)
+                {
+                    _OpponentsPlayField = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("OpponentsPlayField"));
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
